4ab9fb46c358cf8593c9fe863c76392c1a53e235
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2021-09-09 14:48:59 -0400

    Unit One initial commit

---
+++ Unit One - Introduction to Computer Science/src/week1/ExampleTwo.java
@@ -0,0 +1,18 @@
+package week1;
+
+public class ExampleTwo {
+  public static void main(String[] args) {
+    System.out.println("A"); // "A" with enter
+    System.out.print("B"); // "B"
+    System.out.print("C"); // "C"
+    System.out.println("D"); // "D" with enter
+    System.out.println("E"); // "E" with enter
+
+    // String literals are the things in double quotes "Hello" is String literal
+    // A sequence of characters
+    /**
+     * A BCD E
+     * 
+     */
+  }
+}
