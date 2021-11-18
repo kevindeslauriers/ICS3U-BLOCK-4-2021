fa66b93da3c5ca2c19d38f176a2ec692df5d71fd
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2021-11-18 13:53:21 -0500

    Ap FRQ

---
+++ Unit Six - Arrays and ArrayLists/src/week11/HorseBarn.java
@@ -0,0 +1,33 @@
+package week11;
+
+public class HorseBarn {
+   /**
+    * The spaces in the barn. Each array element holds a reference to the horse
+    * that is currently occupying the space. A null value indicates an empty space.
+    */
+   private Horse[] spaces;
+
+   /**
+    * Returns the index of the space that contains the horse with the specified
+    * name. Precondition: No two horses in the barn have the same name.
+    * 
+    * @param name the name of the horse to find
+    * @return the index of the space containing the horse with the specified name;
+    *         -1 if no horse with the specified name is in the barn.
+    */
+   public int findHorseSpace(String name) {
+
+   }
+
+   /**
+    * Consolidates the barn by moving horses so that the horses are in adjacent
+    * spaces, starting at index 0, with no empty space between any two horses.
+    * Postcondition: The order of the horses is the same as before the
+    * consolidation.
+    */
+   public void consolidate() {
+
+   }
+   // There may be instance variables, constructors, and methods that are not
+   // shown.
+}
